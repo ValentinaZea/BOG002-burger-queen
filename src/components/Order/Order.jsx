d6aac27ee2dd453023007f39b1d60f803b3f547e
NataliaG9806 <garavitobarretonatalia@gmail.com>
2021-07-08 12:38:14 -0500

    cambio: logica adiciones hamburguesa(product.jsx). Nuevo: selector mesas(order.jsx), estilos adiciones hamburguesa(product.modele.scss)

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React,{ useState} from 'react';
+import styles from './order.module.scss';
 
 function Order(props) {
-    //console.log(props.sendProduct)
-
+        const [table, setTable] = useState('mesa1');
+        // const [messageList, setMessageList] = useState([]);
+        //console.log(props.sendProduct[3]);
+        function handleChange(e){
+            setTable(e.target.value)
+          //  console.log(e.target.value)
+        }
         return (
             <div>
                 <h1>ORDEN</h1>
                 <hr />
                 <div>
+                    <input/>
+                    <select value={table} onChange={(event)=>handleChange(event)}>
+                        <option value="mesa1">Mesa 1</option>
+                        <option value="mesa2">Mesa 2</option>
+                        <option value="mesa3">Mesa 3</option>
+                        <option value="mesa4">Mesa 4</option>
+                    </select>
+                </div>
+                <div className={styles.bodyOrder}>
                     <p> Item: {props.sendProduct[0]}</p>
                     <p> precio: {props.sendProduct[1]}</p>
+                    <p> tipo: {props.sendProduct[2]}</p>
+                    <p> adicion : {props.sendProduct[3]}</p>
+                </div>
+                <div className={styles.buttonsDown}>
+                    <button className={styles.buttonCancelOrder}>
+
+                    </button>
+                    <button className={styles.buttonSendToKitchen}>
+
+                    </button>
                 </div>
             </div>
         )
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,12 +1,15 @@
 import React, { useState }from 'react'
 import styles from './product.module.scss'
-import menu from '../../menu.json'
-import addProductRed from '../../assets/images/add_product_red.png'
-import addProductYellow from '../../assets/images/add_product_yellow.png'
+import menu from '../../menu.json';
 
 function Product(props) {
-    let categoryData, backgroundColor, addproduct;
+    let categoryData, backgroundColor, addproduct, formHamburguer,  stylesOptions, addButton; 
+    const [typeHamburguer, setTypeHamburger] = useState("Res");
+    const [additionsSimple, setAdditionsSimple] = useState({ Huevo: false , Queso: false});
+    const [additionsDoble, setAdditionsDoble] = useState({ Huevo: false , Queso: false});
     if (props.category === "breakfast"){
+        formHamburguer = styles.noOptionsHamburger;
+        stylesOptions = styles.priceAddDivNoOptions;
         backgroundColor = styles.productCardBreakfast;
         addproduct = styles.addProductBreakfast;
         switch (props.subCategory) {
@@ -24,39 +27,88 @@ function Product(props) {
         }        
     }
     else {
-        backgroundColor = styles.productCardLunch;
         addproduct = styles.addProductLunch;
         switch (props.subCategory) {
             case "firstCategory":
                 categoryData = menu.Almuerzo[0]['Hamburguesas'];
+                formHamburguer = styles.optionsHamburger;
+                stylesOptions = styles.priceAddDivOptions
+                backgroundColor = styles.productCardLunchHamburger;
+                addButton = styles.noOptionsHamburger;
                 break;
             case "secondCategory":
                 categoryData = menu.Almuerzo[0]['Acompañamientos'];
+                formHamburguer = styles.noOptionsHamburger;
+                stylesOptions = styles.priceAddDivNoOptions;
+                backgroundColor = styles.productCardLunch;
                 break;
             case "thirdCategory":
                 categoryData = menu.Almuerzo[0]['Bebidas'];
+                formHamburguer = styles.noOptionsHamburger;
+                stylesOptions = styles.priceAddDivNoOptions
+                backgroundColor = styles.productCardLunch;
                 break;
             default:
                 break;
         }
     }
     function addProduct(item, precio){
-        const sendArray = [item, precio];
+        //console.log(additions)
+        console.log("additions simple " + additionsSimple.Huevo + additionsSimple.Queso)
+        console.log("additions doble " + additionsDoble.Huevo + additionsDoble.Queso)
+        let sendArray = [];
+        if(item.includes('Hamburguesa')){
+            let addition = item.includes('Simple') ? additionsSimple : additionsDoble;
+            let arrayAdditions = [];
+            for (const prop in addition) {
+                if(addition[prop] === true) 
+                arrayAdditions.push(prop);
+            }
+        sendArray = arrayAdditions.length >0 ? [item, precio, typeHamburguer, arrayAdditions] : [item, precio, typeHamburguer]
+        }
+        else{
+            sendArray = [item, precio];
+        }
         props.product(sendArray);
-        //console.log(product)
     }
+    const handleOnChange = (event) => {
+        let set = event.target.id.includes('Simple') ? setAdditionsSimple : setAdditionsDoble;
+        let addition = event.target.id.includes('Simple') ? additionsSimple : additionsDoble;
+        let toggle = event.target.checked ? true : false;
+            if(event.target.value === "Huevo"){
+                set({...addition, Huevo: toggle });
+            }
+            else{
+                set({...addition, Queso: toggle });
+            }    
+    };
     return (
         <div className={styles.menuContainer}>
             {categoryData.map((data, key) => {
             return (
-                <div key={key} className={backgroundColor}>
+                <div key={key} className={backgroundColor} >
                     <div className={styles.imageItemDiv}>
                         <img src={data.Imagen} alt="ProductImage" className={styles.productImage}/>
                         <p className={styles.textItem}>{data.Item}</p>
                     </div>
-                    <div className={styles.priceAddDiv}>
+                    <div className={stylesOptions}>
                         <p className={styles.textPrice}>${data.Precio}</p>
-                        <button className={addproduct} onClick={() => addProduct(data.Item, data.Precio)}></button>
+                        <form id={formHamburguer}>
+                            <div className={styles.containerRadioButton}>
+                                <div className={styles.inputRadioButton}><input onChange={() => setTypeHamburger("Res")} type="radio" value="Res" name="typeHamburguer" defaultChecked/> Res </div>
+                                <div className={styles.inputRadioButton}><input onChange={() => setTypeHamburger("Pollo")} type="radio" value="Pollo" name="typeHamburguer" /> Pollo </div>
+                                <div className={styles.inputRadioButton}><input onChange={() => setTypeHamburger("Vegetariana")} type="radio" value="Vegetariana" name="typeHamburguer" /> Vegetariana </div>
+                            </div>
+                            <div className={styles.textAdditions}><p>Adiciones</p><p>$1</p></div>
+                            <div className={styles.containerCheckAdd}>
+                                <div className={styles.containerCheckBox}>
+                                    <div><input id={"adicion huevo "+ data.Item} onChange={(event) => handleOnChange(event)} type="checkbox" value="Huevo" name="typeAdition" /> Huevo </div>                                
+                                    <div><input id={"addition queso "+ data.Item} onChange={(event) => handleOnChange(event)} type="checkbox" value="Queso" name="typeAdition" /> Queso </div>
+                                </div>                                
+                                <button className={addproduct} type="button" onClick={() => addProduct(data.Item, data.Precio)}></button>
+                            </div>
+                        </form>
+                        <button id={addButton} className={addproduct} onClick={() => addProduct(data.Item, data.Precio)}></button>
                     </div>
                 </div>
             );
